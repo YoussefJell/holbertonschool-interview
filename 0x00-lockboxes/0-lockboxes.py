d99fd45af9383784dd8492671db0ced1473d0c93
YoussefJell <youssef.jellouli@yahoo.com>
2022-12-07 10:06:53 +0000

    add

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    unlocked = list(boxes[0])
+
+    for i in range(0, len(boxes) - 1):
+        for key in boxes[i]:
+            for elem in boxes[key]:
+                unlocked.append(elem)
+    if 0 in unlocked:
+        if len(set(unlocked)) == len(boxes):
+            return True
+    else:
+        if len(set(unlocked)) == len(boxes) - 1:
+            return True
+    return False

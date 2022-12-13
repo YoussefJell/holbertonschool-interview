c0dee8e6bca09af3c41e4175d67fc45933ef8c78
YoussefJell <youssef.jellouli@yahoo.com>
2022-12-13 12:02:03 +0100

    add

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * @number: number
+ * @head: head of list
+ * Return: Always 0.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new;
+    listint_t *tmp;
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    if (*head == NULL)
+        return (NULL);
+
+    tmp = *head;
+
+    new->n = number;
+    new->next = NULL;
+
+    while ((tmp != NULL) && (new->n > tmp->next->n))
+        tmp = tmp->next;
+
+    if (tmp)
+    {
+        new->next = tmp->next;
+        tmp->next = new;
+        return (new);
+    }
+    return (NULL);
+}

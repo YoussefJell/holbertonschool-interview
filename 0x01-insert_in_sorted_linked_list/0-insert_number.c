341dbbb3c96031d1df56eb6516c07ff15d990c0e
YoussefJell <youssef.jellouli@yahoo.com>
2022-12-15 10:38:06 +0000

    add

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -20,7 +20,7 @@ listint_t *insert_node(listint_t **head, int number)
     new->n = number;
     new->next = NULL;
 
-    if (*head == NULL)
+    if ((*head)->n >= new->n || *head == NULL)
     {
         new->next = *head;
         *head = new;

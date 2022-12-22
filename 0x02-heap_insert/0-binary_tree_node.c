9a80d3f7422778add79072b73f8480385366e3e6
YoussefJell <youssef.jellouli@yahoo.com>
2022-12-22 13:05:22 +0000

    fix betty

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -4,25 +4,25 @@
  *
  * @parent: Pointer to the parent node
  * @value: value of n
+ * Return: new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node;
+	binary_tree_t *node;
 
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
 
-    node = malloc(sizeof(binary_tree_t));
-    if (node == NULL)
-        return (NULL);
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
 
-    node->n = value;
-    node->left = NULL;
-    node->right = NULL;
-
-    if (parent == NULL)
-    {   
-        node->parent = NULL;
-        return (node);
-    }
-    node->parent = parent;
-    return (node);
+	if (parent == NULL)
+	{
+		node->parent = NULL;
+		return (node);
+	}
+	node->parent = parent;
+	return (node);
 }

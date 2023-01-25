fa97389dbc54e9be4188f410ae53715d4e78e2e0
YoussefJell <youssef.jellouli@yahoo.com>
2023-01-25 09:48:30 +0000

    add

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,25 +2,30 @@
 import sys
 
 
-def recap(codeMap, fileSize):
-    print('File size: {}'.format(fileSize))
+count = 1
+statusCodeMap = {}
+fileSizes = []
+
+
+def recap(codeMap, fileSizes):
+    print('File size: {}'.format(fileSizes))
     for key, val in sorted(codeMap.items()):
         print('{}: {}'.format(key, val))
 
 
-count = 1
-statusCodeMap = {}
-fileSizes = []
-for line in sys.stdin:
-    newList = line.split(" ")
-    try:
-        fileSizes.append(int(newList[-1]))
-    except:
-        continue
-    statusCodeMap[newList[-2]] = statusCodeMap.get(newList[-2], 0) + 1
-    if count % 10 == 0:
-        fileSize = sum(fileSizes)
-        recap(statusCodeMap, fileSize)
-        statusCodeMap = {}
-        fileSizes = []
-    count += 1
+try:
+    for line in sys.stdin:
+        newList = line.split(" ")
+        try:
+            fileSizes.append(int(newList[-1]))
+        except:
+            continue
+        statusCodeMap[newList[-2]] = statusCodeMap.get(newList[-2], 0) + 1
+        if count % 10 == 0:
+            recap(statusCodeMap, sum(fileSizes))
+            statusCodeMap = {}
+            fileSizes = []
+        count += 1
+except KeyboardInterrupt:
+    recap(statusCodeMap, sum(fileSizes))
+    raise

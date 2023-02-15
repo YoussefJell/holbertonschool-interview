9860e7260c326461f6adedd7894475f6ea23d865
YoussefJell <youssef.jellouli@yahoo.com>
2023-02-15 21:09:41 +0000

    add

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -7,16 +7,16 @@ def validUTF8(data):
     count = 0
     for c in data:
         if (count == 0):
-            if (c == 0b11000000):
+            if ((c >> 5) == 0b110):
                 count = 1
-            elif (c == 0b11100000):
+            elif ((c >> 4) == 0b1110):
                 count = 2
-            elif (c == 0b11110000):
+            elif ((c >> 3) == 0b11110):
                 count = 3
             elif ((c >> 7)):
                 return False
         else:
-            if (c != 0b10000000):
+            if ((c >> 6) != 0b10):
                 return False
             count -= 1
 

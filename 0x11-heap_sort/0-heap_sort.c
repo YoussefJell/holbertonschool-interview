f36650999e68652a534a87cd6d590a96a40acee6
YoussefJell <youssef.jellouli@yahoo.com>
2023-04-27 18:31:05 +0000

    add

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -8,10 +8,9 @@
  */
 void swap(int *a, int *b)
 {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
-
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 /**
  * sift_down - sifts down
@@ -23,26 +22,27 @@ void swap(int *a, int *b)
  */
 void sift_down(int *array, int root, int end)
 {
-  int child, temp;
-  temp = array[root];
-  while ((2 * root) + 1 <= end)
-  {
-    child = 2 * root + 1;
-    if (child + 1 <= end && array[child] < array[child + 1])
-    {
-      child++;
-    }
-    if (temp < array[child])
-    {
-      array[root] = array[child];
-      root = child;
-    }
-    else
-    {
-      break;
-    }
-  }
-  array[root] = temp;
+	int child, temp;
+
+	temp = array[root];
+	while ((2 * root) + 1 <= end)
+	{
+		child = 2 * root + 1;
+		if (child + 1 <= end && array[child] < array[child + 1])
+		{
+			child++;
+		}
+		if (temp < array[child])
+		{
+			array[root] = array[child];
+			root = child;
+		}
+		else
+		{
+			break;
+		}
+	}
+	array[root] = temp;
 }
 /**
  * heap_sort - heap sort
@@ -53,15 +53,16 @@ void sift_down(int *array, int root, int end)
  */
 void heap_sort(int *array, size_t size)
 {
-  int i;
-  for (i = (size / 2) - 1; i >= 0; i--)
-  {
-    sift_down(array, i, size - 1);
-  }
-  for (i = size - 1; i >= 1; i--)
-  {
-    swap(&array[0], &array[i]);
-    print_array(array, size);
-    sift_down(array, 0, i - 1);
-  }
+	int i;
+
+	for (i = (size / 2) - 1; i >= 0; i--)
+	{
+		sift_down(array, i, size - 1);
+	}
+	for (i = size - 1; i >= 1; i--)
+	{
+		swap(&array[0], &array[i]);
+		print_array(array, size);
+		sift_down(array, 0, i - 1);
+	}
 }

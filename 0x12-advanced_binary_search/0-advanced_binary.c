8660636b963ec719213b5d03c10781c2b0f7bb1d
YoussefJell <youssef.jellouli@yahoo.com>
2023-05-04 17:07:00 +0000

    add

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+/**
+ * advanced_binary_recursive - uses low and high for binary search
+ *
+ * @array: arr
+ * @low: low
+ * @high: high
+ * @value: value
+ * Return: value
+ */
+int advanced_binary_recursive(int *array, int low, int high, int value)
+{
+	if (low > high)
+		return (-1);
+
+	int mid = (low + high) / 2;
+
+	printf("Searching in array: ");
+	for (int i = low; i <= high; i++)
+		printf("%d%s", array[i], i == high ? "\n" : ", ");
+
+	if (array[mid] == value)
+	{
+		if (mid == low || array[mid - 1] != value)
+			return (mid);
+		else
+			return (advanced_binary_recursive(array, low, mid, value));
+	}
+	else if (array[mid] > value)
+		return (advanced_binary_recursive(array, low, mid - 1, value));
+	else
+		return (advanced_binary_recursive(array, mid + 1, high, value));
+}
+/**
+ * advanced_binary - binary search recursively
+ *
+ * @array: array
+ * @size: size
+ * @value: value
+ * Return: int
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (!array || size == 0)
+		return (-1);
+
+	return (advanced_binary_recursive(array, 0, size - 1, value));
+}

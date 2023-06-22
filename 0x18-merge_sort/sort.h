acfe869c8fd99c85361b3d1384efed20b5ce66ae
YoussefJell <youssef.jellouli@yahoo.com>
2023-06-22 22:16:39 +0000

    add

---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_array(const int *array, size_t size);
+void merge(int *array, int left[], int left_size, int right[], int right_size);
+
+#endif

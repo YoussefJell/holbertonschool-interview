96971069122dc56e10ec04fd82d466b66c5a0cdb
YoussefJell <youssef.jellouli@yahoo.com>
2023-07-06 23:06:05 +0000

    add

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+module makeChange
+"""
+
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
+
+    newVal = total + 1
+    store = {0: 0}
+
+    for i in range(1, total + 1):
+        store[i] = newVal
+
+        for coin in coins:
+            current = i - coin
+            if current < 0:
+                continue
+
+            store[i] = min(store[current] + 1, store[i])
+
+    if store[total] == total + 1:
+        return -1
+
+    return store[total]
